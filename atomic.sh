914fc3ac61204bf60738a51495f57292cbb9a13c
Jonathan Lebon <jlebon@users.noreply.github.com>
2016-04-05 22:17:44 -0400

    Update atomic.sh Closes: #4 Approved by: jlebon

--- atomic.sh
+++ atomic.sh
@@ -1,4 +1,3 @@
-#
 # Licensed under the GNU General Public License Version 2
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
